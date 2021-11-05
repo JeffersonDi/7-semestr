bd282d498c618eaeb0547e8bba187f5adde45c5e
Jefd <djordon197@gmail.com>
2021-11-05 19:04:44 +0700

    БД

---
+++ Shopping/Infrastructure/ShoppingContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Infrastructure
+{
+    public class ShoppingContext : DbContext //CmsShoppingCartContext=>ShoppingContext
+    {
+        public ShoppingContext(DbContextOptions<ShoppingContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}

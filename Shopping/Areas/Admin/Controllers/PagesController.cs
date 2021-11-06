420af01cc1f369903647da0763d09a9933f8c584
Jefd <djordon197@gmail.com>
2021-11-07 02:02:23 +0700

    #реализован PageController

--- Shopping/Areas/Admin/Controllers/PagesController.cs
+++ Shopping/Areas/Admin/Controllers/PagesController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shopping.Infrastructure;
+using Shopping.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +12,20 @@ namespace Shopping.Areas.Admin.Controllers
     [Area("Admin")]
     public class PagesController : Controller
     {
-        public string Index()
+        private readonly ShoppingContext context;
+
+        public PagesController(ShoppingContext context)
         {
-            return "test";
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IQueryable<Page> pages = from p in context.Pages orderby p.Sorting select p;
+
+            List<Page> pagesList = await pages.ToListAsync();
+
+            return View(pagesList);
         }
     }
 }

bba6c56f1821489832ccfb2b70c84f49ffa66ce5
Jefd <djordon197@gmail.com>
2021-11-08 00:45:24 +0700

    #реализованы функции: удаления, редактирования, добавления, подробности

--- Shopping/Areas/Admin/Controllers/PagesController.cs
+++ Shopping/Areas/Admin/Controllers/PagesController.cs
@@ -46,6 +46,7 @@ namespace Shopping.Areas.Admin.Controllers
 
         //POST /admin/pages/create
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Page page)
         {
             if (ModelState.IsValid)
@@ -63,10 +64,70 @@ namespace Shopping.Areas.Admin.Controllers
                 context.Add(page);
                 await context.SaveChangesAsync();
 
+                TempData["Success"] = "Запись успешно добавлена!";
+
                 return RedirectToAction("Index");
             }
 
             return View(page);
+        }
+
+        //GET /admin/pages/edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            Page page = await context.Pages.FindAsync(id);
+            if (page == null)
+            {
+                return NotFound();
+            }
+
+            return View(page);
+        }
+
+        //POST /admin/pages/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Page page)
+        {
+            if (ModelState.IsValid)
+            {
+                page.Slug = page.Id == 1 ? "home" : page.Title.ToLower().Replace(" ", "-");
+
+                var slug = await context.Pages.Where(x => x.Id != page.Id).FirstOrDefaultAsync(x => x.Slug == page.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "Этот заголовок уже существует.");
+                    return View(page);
+                }
+
+                context.Update(page);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "Запись успешно добавлена!";
+
+                return RedirectToAction("Edit", new { id = page.Id });
+            }
+
+            return View(page);
+        }
+
+        //GET /admin/pages/delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            Page page = await context.Pages.FindAsync(id);
+            if (page == null)
+            {
+                TempData["Error"] = "Страница не существует!";
+            }
+            else
+            {
+                context.Pages.Remove(page);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "Запись успешно удалена!";
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }

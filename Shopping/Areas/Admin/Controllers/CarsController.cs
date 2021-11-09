46797b6eb6a590128204fd915515485f245ae02b
Jefd <djordon197@gmail.com>
2021-11-10 00:09:01 +0700

    #полностью реализована страница Автомобили

--- Shopping/Areas/Admin/Controllers/CarsController.cs
+++ Shopping/Areas/Admin/Controllers/CarsController.cs
@@ -36,6 +36,18 @@ namespace Shopping.Areas.Admin.Controllers
             return View(await cars.ToListAsync());
         }
 
+        //GET /admin/cars/details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            Car car = await context.Cars.Include(x => x.Category).FirstOrDefaultAsync(x => x.Id == id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            return View(car);
+        }
+
         //GET /admin/cars/create
         public IActionResult Create() 
         {
@@ -72,9 +84,10 @@ namespace Shopping.Areas.Admin.Controllers
                     FileStream fs = new FileStream(filePath, FileMode.Create);
                     await car.ImageUpload.CopyToAsync(fs);
                     fs.Close();
+                    car.Image = imageName;
                 }
 
-                car.Image = imageName;
+                //car.Image = imageName;
 
                 context.Add(car);
                 await context.SaveChangesAsync();
@@ -87,16 +100,100 @@ namespace Shopping.Areas.Admin.Controllers
             return View(car);
         }
 
-        //GET /admin/cars/details/5
-        public async Task<IActionResult> Details(int id)
+        //GET /admin/cars/edit/5
+        public async Task<IActionResult> Edit(int id)
         {
-            Car car = await context.Cars.Include(x => x.Category).FirstOrDefaultAsync(x => x.Id == id);
+            Car car = await context.Cars.FindAsync(id);
+
             if (car == null)
             {
                 return NotFound();
             }
 
+            ViewBag.CategoryId = new SelectList(context.Categories.OrderBy(x => x.Sorting), "Id", "Name", car.CategoryId);
             return View(car);
+        }
+
+        //POST /admin/cars/edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Car car)
+        {
+            ViewBag.CategoryId = new SelectList(context.Categories.OrderBy(x => x.Sorting), "Id", "Name", car.CategoryId);
+
+            if (ModelState.IsValid)
+            {
+                car.Slug = car.Name.ToLower().Replace(" ", "-");
+                //car.Sorting = 100;
+
+                var slug = await context.Cars.Where(x => x.Id != id).FirstOrDefaultAsync(x => x.Slug == car.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "Эта позиция уже существует.");
+                    return View(car);
+                }
+               
+                if (car.ImageUpload != null)
+                {
+                    string uploadsDir = Path.Combine(webHostEnvironment.WebRootPath, "media/cars");
+
+                    if(!string.Equals(car.Image, "noimage.png"))
+                    {
+                        string oldImagePath = Path.Combine(uploadsDir, car.Image);
+
+                        if(System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    string imageName = Guid.NewGuid().ToString() + "_" + car.ImageUpload.FileName;
+                    string filePath = Path.Combine(uploadsDir, imageName);
+                    FileStream fs = new FileStream(filePath, FileMode.Create);
+                    await car.ImageUpload.CopyToAsync(fs);
+                    fs.Close();
+                    car.Image = imageName;
+                }
+
+                context.Update(car);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "Запись успешно изменена!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(car);
+        }
+
+        //GET /admin/cars/delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            Car car = await context.Cars.FindAsync(id);
+            if (car == null)
+            {
+                TempData["Error"] = "Записи не существует!";
+            }
+            else
+            {
+                if (!string.Equals(car.Image, "noimage.png"))
+                {
+                    string uploadsDir = Path.Combine(webHostEnvironment.WebRootPath, "media/cars");
+                    string oldImagePath = Path.Combine(uploadsDir, car.Image);
+
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
+
+                context.Cars.Remove(car);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "Запись успешно удалена!";
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }

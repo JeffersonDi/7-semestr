adff5baa2401a0accf0ec9d351387b9c8098664d
Jefd <djordon197@gmail.com>
2021-11-17 06:30:23 +0700

    #баги

--- Shopping/Controllers/CarsController.cs
+++ Shopping/Controllers/CarsController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Shopping.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class CarsController : Controller
     {
         private readonly ShoppingContext context;

e12eff3fd2c382af7423b29fb16f5f4ad8088a95
Jefd <djordon197@gmail.com>
2021-12-23 23:30:54 +0700

    #опросник feedback

---
+++ Shopping/Controllers/FeedbackController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.Models;
+
+namespace Shopping.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        [HttpPost]
+        public JsonResult SendFeedback(FeedbackModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // отправляем сообщение...
+                return Json(new { result = "Сообщение отправлено" });
+            }
+            else
+            {
+                return Json(new { error = "ошибка в заполнении формы" });
+            }
+        }
+    }
+}
---
+++ Shopping/Models/FeedbackModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Models
+{
+    public class FeedbackModel
+    {
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Тема сообщения")]
+        public string Subject { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Как к Вам обращаться")]
+        public string UserName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Неверный формат электронной почты")]
+        [StringLength(50)]
+        [Display(Name = "Email для обратной связи")]
+        public string EmailAdrress { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Текст сообщения")]
+        public string Message { get; set; }
+
+    }
+}
